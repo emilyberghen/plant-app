44219907b6a126bab88c32150c783d0a81282dc8
Emily <emily.berghen@hotmail.com>
2020-12-13 23:55:55 +0100

    netlify index.html

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     module: {
     rules: [
         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-        { test: /\.css$/, use: 'css-loader' }
+        { test: /\.css$/, use: [ 'style-loader','css-loader' ] }
     ]
     }
 };
